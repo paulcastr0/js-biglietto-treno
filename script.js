9f9fcc1608f0936896e81a40b6eab74d19ccbdc5
paulcastr0 <castropaul.learn@gmail.com>
2022-11-10 16:51:41 +0100

    Completamento Fase HTML

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//Input da inserire
 let kilometers = prompt("How many kilometers do you have to do? ");
 let age = prompt("How old are you? ");
 
 let ticketPrice = kilometers * 0.21 ;
 
+//arrotondamento fino a seconda cifra decimale
 ticketPrice = (Math.round(ticketPrice * 100)/100).toFixed(2);
 
+//operazione per il calcolo del
 if (age < 18) {
     console.log(ticketPrice * 80/100)
 }
@@ -13,4 +16,4 @@ else if (age > 65) {
 }
 else {
     console.log(ticketPrice)
-}
+} 
